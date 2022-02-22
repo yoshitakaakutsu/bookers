3ac5dc2fe80a3029947fe23e6cff4f577626cca5
EC2 Default User <ec2-user@ip-172-31-38-48.ec2.internal>
2022-02-22 06:28:58 +0000

    checksheet28

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   get '/' => "homes#top"
   get '/books' => "books#index"
   post'/book' => "books#create"
-  get'/books/show' => "books#show"
-  get'/show' => "books#show"
-  get'/edit' => "books#edit"
-  get'/destroy' => "books#destroy"
+  get'/books/:id' => 'books#show',as:'show'
+  get'/books/:id/edit' => "books#edit",as:'edit'
+  patch'/book/:id' => "books#update",as:'update'
+  delete'books/:id' => "books#destroy",as:'destroy'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
